a8a9df2f37b46a0e363d8df880e8a9ecf59af08f
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-10-13 23:04:55 -0400

    added modal for city search validation

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,1 +1,12 @@
 https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=40799473c0f8af4234bae3f870dc5940
+
+var citySearch = $('#weatherForm');
+var submitBtn = $('#submitBtn');
+citySearch.on('submit',displayWeather);
+console.log(citySearch)
+
+function displayWeather(event){
+    event.preventDefault();
+    console.log(event.target[0].value)
+    $('#exampleModal').modal('show');
+}
