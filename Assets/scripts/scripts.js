e4a35eb50d8a19d30dc87ebf5bbc48a619eb1068
Dasantia89 <143134358+Dasantia89@users.noreply.github.com>
2023-10-15 20:20:47 -0400

    wrote function to handle event

--- Assets/scripts/scripts.js
+++ Assets/scripts/scripts.js
@@ -1,38 +1,39 @@
-var key = '40799473c0f8af4234bae3f870dc5940'
+var key = '0015257cf3d37453b04b0cfba52caeed'
 var forecast = 'https://api.openweathermap.org/data/2.5/forecast'; 
-// lat={lat}&lon={lon}
+var weather = 'https://api.openweathermap.org/data/2.5/weather';
 var geocode = 'http://api.openweathermap.org/geo/1.0/direct';
 var citySearch = $('#weatherForm');
 var submitBtn = $('#submitBtn');
 var stateSelect = $('#stateSelect');
+const today = [];
+var cities = [];
 var states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
 "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
 "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
 "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
 "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-
+var stateCodes = [
+    "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+    "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+    "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+  ];
 for(var x=0; x<states.length;x++){
     var state = states[x];
-    var option = $(`<option value="${state}">${state}</option>`);
+    var code = stateCodes[x]
+    var option = $(`<option value="${code}">${state}</option>`);
     stateSelect.append(option)
 }
 
 citySearch.on('submit',displayWeather);
+
 function displayWeather(event){
-    event.preventDefault();
-    console.log(event.target[0].value)
-    var latLong = getLatLong(event.target[0].value);
-    // $('#exampleModal').modal('show');
+    event.preventDefault();    
+    if(event.target[1].value == 'State' || event.target[0].value.length<1){
+    $('#exampleModal').modal('show');
+    }else{
+        var latLong;
+        latLong = getLatLong(event.target[0].value,event.target[1].value);   
+        console.log(today)     
+    }
 }
 
-function getLatLong (city){
-    var link = geocode + '?q=' + city + '&limit=1&appid=' + key;
-    console.log(link)
-    fetch(link, )
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-}
